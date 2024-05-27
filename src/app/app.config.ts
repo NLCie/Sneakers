853274a50dfca870640299669e0c77305c829af9
nlcie <nciellalally@gmail.com>
2024-05-27 14:03:07 +0200

    initializing the exercices of ngxs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,9 +6,11 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { RecupApiService } from './Services/recup-api.service';
 import { InterceptorService } from './Interceptors/interceptor.service';
+import { NgxsModule } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()
   ,{ provide: RecupApiService}, provideHttpClient(), { provide: InterceptorService}, 
+  importProvidersFrom(NgxsModule.forRoot([]))
   ]
 };
